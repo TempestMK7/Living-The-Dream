6f8e2f851f9a676d3edfa4921d123fab3670138a
DavidCG <david.greer@hcss.com>
2019-01-05 11:12:28 -0600

    Added outfits to explorers.

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -9,7 +9,7 @@ namespace Com.Tempest.Nightmare {
         public const string ROOM_NEEDS_EXPLORERS = "room_needs_explorers";
         public const string ROOM_NEEDS_NIGHTMARES = "room_needs_nightmares";
 
-        public const string GAME_VERSION = "0.17";
+        public const string GAME_VERSION = "0.18";
 
         public const string SAVE_FILE_NAME = "LlomSaveData_" + GAME_VERSION + ".txt";
         public const string CONTROL_FILE_NAME = "LlomControlData_" + GAME_VERSION + ".txt";
